23bb8728a52a12eec0278ec826c2a05285c89472
Juan Marcos Mendez <jmendez50@mail.valenciacollege.edu>
2016-03-23 23:25:50 -0400

    Mid Juan Mendez
    Juan Mendez Midterm. Read comments in q2

--- q2.py
+++ q2.py
@@ -1,11 +1,8 @@
-# This is a very simple game of sticks. There are 21 sticks, first the user picks number of sticks between 1-4, then the computer picks sticks(1-4). Who ever will pick the last stick will lose.
-# Look for the TODO blocks as an indication of when you have to add your own code.
-
+#Juan Marcos Mendez
 import random
 sticks = 21
 
 def playGame():
-    # you do not need to modify code in this function
     print('There are 21 sticks. You can pick from 1 to 4 sticks at a time, and the computer will then pick from 1 to 4 sticks at a time. Whoever picks the last stick loses!')
     while True:
         print('Current sticks: ' + str(sticks))
@@ -19,22 +16,26 @@ def playGame():
         if subtractSticks( computerChoice ):
             print('Computer lost!')
             break
-        
-
-def askUserChoice():
-    # TODO: write code in this functiont that:
-    # 1. Asks the user to enter their input (between 1 and 4)
-    # 2. Checks that the user's input is valid. If it's not valid (if it's not between 1 and 4), then ask the user to re-enter their input.
-    # 3. Once the user enters a valid input, return that input as an integer.
-
-
-def subtractSticks( number ):
-    global sticks 
-    
-    # TODO: write code inside this function that:
-    # 1. subtracts the parameter `number` from the global variable `sticks`
-    # 2. checks if the number subtracted resulted in the last stick, if so, return True
-    # 3. if there are still sticks left, return False
-    
-def determineComputerChoice():
-    # TODO: write code inside this function that returns an integer between 1 and 4, random chosen by the computer
+#################################################        
+def askUserChoice():                            #
+    numbers = [1,2,3,4]                         # list of number only allowed
+    user = ''                                   # user pick
+    while user not in numbers:                  # Saying user can only pick from these numbers
+        print('Pick a number. Only 1-4')        # Display
+        user = int(input())                     # Asking user to pick
+        if user not in numbers:                 # If user didnt pick right then
+            print('OOPS try again. 1-4 only')   # Display
+        else:                                   # if worked then
+            return int(user)                    # do this
+def subtractSticks( number ):                   #
+    global sticks                               #
+    sticks = sticks - number                    # what was asked for
+    if sticks <= 0:                             # if sticks is zero
+        return True                             # then ture you win
+    else:                                       # or 
+        return False                            # lose
+def determineComputerChoice():                  #
+    computerChoice = random.randint(1,4)        # computer random number picking
+    return computerChoice                       # return the number to your code part
+#For the the computer random number i had to look online didnt know it.
+#Also i got some advice from people that know this better then i do.
